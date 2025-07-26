500fba61b5917987f6858b5275044c4ceae91747
Rhodri Davies <rhodridavies99@yahoo.com>
2025-07-26 20:53:04 +0100

    Evaluate all models and produce better calibration plots

--- code/model_evaluation.py
+++ code/model_evaluation.py
@@ -2,17 +2,20 @@
 import os
 import pandas as pd
 from configparser import ConfigParser
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 from sklearn.metrics import(roc_auc_score,
                             f1_score,
+                            fbeta_score,
                             recall_score,
                             precision_score,
-                            balanced_accuracy_score,
-                            roc_curve,
                             precision_recall_curve,
                             auc,
                             brier_score_loss,
                             confusion_matrix)
+from imblearn.metrics import geometric_mean_score
+from sklearn.calibration import CalibrationDisplay
 
 from ModelEvaluation import ModelEvaluator
 from utils import helpers as hlp
@@ -34,57 +37,63 @@ uhbw_suffix = config.get('global', 'uhbw_data')
 data_filetype = config.get('global', 'data_filetype')
 
 model_filetype = config.get('model_development', 'models_filetype')
-base_model_filename = config.get('model_development', 'base_model_filename')
-best_model_filename = config.get('model_development', 'best_model_filename')
+lreg_model_filename = config.get('model_development', 'lreg_model_filename')
+rfc_model_filename = config.get('model_development', 'rfc_model_filename')
+xgb_model_filename = config.get('model_development', 'xgb_model_filename')
+lgbm_model_filename = config.get('model_development', 'lgbm_model_filename')
+
+n_bins = int(config.get('evaluation', 'n_bins_calibration'))
 
 ml_metrics_path = config.get('evaluation', 'ml_metrics_path')
 
 
 def evaluate_models(suffix, d_type):
 
-    LOGGER.info(f"Evaluating the base model for the {d_type} data")
+    model_names = ['Logistic Regression',
+                   'Random Forest',
+                   'XGBoost',
+                   'LightGBM']
+    filepaths = [lreg_model_filename,
+                 rfc_model_filename,
+                 xgb_model_filename,
+                 lgbm_model_filename]
+    
+    eval_dict = {}
+    metrics_list = []
+    
+    for name, path in zip(model_names, filepaths):
 
-    filepath = base_model_filename + suffix + model_filetype
-    base_model = hlp.load_model(filepath, LOGGER)
+        LOGGER.info(f"Evaluating the {name} model for the {d_type} data")
 
-    base_eval = ModelEvaluator(base_model, suffix, config, LOGGER)
-    base_eval.load_data()
-    base_eval.predict_outcomes()
-    base_eval.predict_probabilitess()
-    base_metrics = base_eval.get_ml_metrics()
-    base_eval.plot_roc_pr_curve()
-    base_eval.plot_confusion_matrix()
-    base_eval.plot_coefs()
-    base_eval.plot_shap()
-    base_eval.plot_calibration_curve()
+        filepath = path + suffix + model_filetype
+        model = hlp.load_model(filepath, LOGGER)
 
-    LOGGER.info(f"Evaluating the best tree based model for the {d_type} data")
+        eval_obj = ModelEvaluator(model, suffix, config, LOGGER)
+        eval_obj.load_data()
+        eval_obj.predict_outcomes()
+        eval_obj.predict_probabilitess()
+        metrics = eval_obj.get_ml_metrics()
+        eval_obj.plot_roc_pr_curve()
+        eval_obj.plot_confusion_matrix()
+        eval_obj.plot_shap()
 
-    filepath = best_model_filename + suffix + model_filetype
-    best_model = hlp.load_model(filepath, LOGGER)
-
-    best_eval = ModelEvaluator(best_model, suffix, config, LOGGER)
-    best_eval.load_data()
-    best_eval.predict_outcomes()
-    best_eval.predict_probabilitess()
-    best_metrics = best_eval.get_ml_metrics()
-    best_eval.plot_roc_pr_curve()
-    best_eval.plot_confusion_matrix()
-    best_eval.plot_shap()
-    best_eval.plot_calibration_curve()
+        eval_dict[name] = eval_obj
+        metrics_list.append(metrics)
 
     # join the metrics together for further processing
-    metrics = base_metrics.merge(best_metrics, on='Metrics')
+    metrics = metrics_list[0].merge(metrics_list[1], on='Metrics')
+    for df in metrics_list[2:]:
+        metrics = metrics.merge(df, on='Metrics')
 
     if d_type=='full':
         d_type = 'Full'
 
     metrics.columns = pd.MultiIndex.from_arrays([
-        ['', d_type, d_type],
+        ['', d_type, d_type, d_type, d_type],
         metrics.columns
     ])
 
-    return metrics, base_eval, best_eval
+    return metrics, eval_dict
 
 
 def merge_multiindex(df1, df2):
@@ -112,14 +121,75 @@ def merge_multiindex(df1, df2):
     return merged_df
 
 
+def plot_calibration_curve(eval_dict, dtype, filepath, LOGGER):
+
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    fig = plt.figure(figsize=(10, 10))
+    gs = GridSpec(4, 2)
+    colours = plt.get_cmap('Dark2')
+
+    ax_calibration_curve = fig.add_subplot(gs[:2, :2])
+    calibration_displays = {}
+
+    for i, (name, eval_obj) in enumerate(eval_dict.items()):
+
+        b_score = brier_score_loss(eval_obj.y, eval_obj.y_prob)
+        legend_name = f"{name} (Brier score: {b_score :.2f})"
+
+        display = CalibrationDisplay.from_predictions(
+            eval_obj.y,
+            eval_obj.y_prob,
+            n_bins=n_bins,
+            name=legend_name,
+            ax=ax_calibration_curve,
+            color=colours(i)
+        )
+        calibration_displays[name] = display
+    ax_calibration_curve.grid()
+    ax_calibration_curve.set(
+        title=f'Calibration plots for models trained on the {dtype} data',
+        xlabel='Mean predicted probability',
+        ylabel='Fraction of positives'
+    )
+
+    grid_positions = [(2, 0), (2, 1), (3, 0), (3, 1)]
+    for i, (name, _) in enumerate(eval_dict.items()):
+        row, col = grid_positions[i]
+        ax = fig.add_subplot(gs[row, col])
+
+        ax.hist(
+            calibration_displays[name].y_prob,
+            range=(0, 1),
+            bins=n_bins,
+            label=name,
+            color=colours(i)
+        )
+        ax.set(
+            title=name,
+            xlabel='Mean predicted probability',
+            ylabel='Count'
+        )
+    
+    plt.tight_layout()
+    plt.savefig(filepath)
+    plt.close()
+
+    LOGGER.info(f'Saved calibration plot for the {dtype} data to {filepath}')
+
+    return None
+
+
+
 def eval_metrics(model_name, model_data, test_data, y_true, y_pred, y_pred_proba, LOGGER):
     
         # calculate metrics
-        accuracy = balanced_accuracy_score(y_true, y_pred)
-        b_score = brier_score_loss(y_true, y_pred_proba)
+        g_mean = geometric_mean_score(y_true, y_pred)
         precision_value = precision_score(y_true, y_pred)
         recall_value = recall_score(y_true, y_pred)
         f1 = f1_score(y_true, y_pred)
+        f2 = fbeta_score(y_true, y_pred, beta=2)
+        f05 = fbeta_score(y_true, y_pred, beta=0.5)
         tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
         specificity = tn/(tn+fp)
         roc_auc = roc_auc_score(y_true, y_pred_proba)
@@ -129,16 +199,17 @@ def eval_metrics(model_name, model_data, test_data, y_true, y_pred, y_pred_proba
 
 
         metrics_df = pd.DataFrame({
-            'Model': [f'{model_name} trained on {model_data} data'],
+            'Model': [model_name],
             'Test Data': [test_data],
             'PR-AUC': [round(pr_auc, 3)],
             'ROC-AUC': [round(roc_auc, 3)],
-            'Brier Score': [round(b_score, 3)],
             'F1 Score': [round(f1, 3)],
+            'F2 Score': [round(f2, 3)],
+            'F0.5 Score': [round(f05, 3)],
             'Recall': [round(recall_value, 3)],
             'Precision': [round(precision_value, 3)],
             'Specificity': [round(specificity, 3)],
-            'Weighted Accuracy': [round(accuracy, 3)]
+            'Geometric Mean': [round(g_mean, 3)]
         })
 
         metrics_df = (metrics_df
@@ -193,13 +264,16 @@ def evaluate_model_robustness(information_dict):
 LOGGER.info("Evaluating models...")
 
 # evaluate models for full data
-metrics_full, base_eval_full, best_eval_full = evaluate_models(full_suffix, "full")
+metrics_full, full_eval_dict = evaluate_models(full_suffix, "full")
+plot_calibration_curve(full_eval_dict, 'full', 'results/supplementary_results/calibration_plots_full.png', LOGGER)
 
 # evaluate models for nbt data
-metrics_nbt, base_eval_nbt, best_eval_nbt = evaluate_models(nbt_suffix, "NBT")
+metrics_nbt, nbt_eval_dict = evaluate_models(nbt_suffix, "NBT")
+plot_calibration_curve(nbt_eval_dict, 'NBT', 'results/supplementary_results/calibration_plots_nbt.png', LOGGER)
 
 # evaluate models for uhbw data
-metrics_uhbw, base_eval_uhbw, best_eval_uhbw = evaluate_models(uhbw_suffix, "UHBW")
+metrics_uhbw, uhbw_eval_dict = evaluate_models(uhbw_suffix, "UHBW")
+plot_calibration_curve(uhbw_eval_dict, 'UHBW', 'results/supplementary_results/calibration_plots_uhbw.png', LOGGER)
 
 metrics = merge_multiindex(metrics_full, metrics_nbt)
 metrics = merge_multiindex(metrics, metrics_uhbw)
@@ -212,82 +286,44 @@ hlp.save_to_csv(metrics, ml_metrics_path, LOGGER)
 
 
 # evaluate models against different subset of data
-base_full_nbt_X = base_eval_full.X[base_eval_full.X['site_ip']=='nbt'].copy()
-base_full_nbt_y = base_eval_full.y[base_full_nbt_X.index]
+best_full_nbt_X = full_eval_dict['Random Forest'].X[full_eval_dict['Random Forest'].X['site_ip']=='nbt'].copy()
+best_full_nbt_y = full_eval_dict['Random Forest'].y[best_full_nbt_X.index]
 
-base_full_uhbw_X = base_eval_full.X[base_eval_full.X['site_ip']=='uhbw'].copy()
-base_full_uhbw_y = base_eval_full.y[base_full_uhbw_X.index]
-
-best_full_nbt_X = best_eval_full.X[best_eval_full.X['site_ip']=='nbt'].copy()
-best_full_nbt_y = best_eval_full.y[best_full_nbt_X.index]
-
-best_full_uhbw_X = best_eval_full.X[best_eval_full.X['site_ip']=='uhbw'].copy()
-best_full_uhbw_y = best_eval_full.y[best_full_uhbw_X.index]
+best_full_uhbw_X = full_eval_dict['Random Forest'].X[full_eval_dict['Random Forest'].X['site_ip']=='uhbw'].copy()
+best_full_uhbw_y = full_eval_dict['Random Forest'].y[best_full_uhbw_X.index]
 
 information_dict = {
-    'Full Logistic Regression on NBT': {
-        'model_name': 'Logistic Regression',
-        'model_data': 'Full',
-        'test_data': 'NBT',
-        'y_true': base_full_nbt_y,
-        'y_pred': base_eval_full.pipe.predict(base_full_nbt_X),
-        'y_prob': base_eval_full.pipe.predict_proba(base_full_nbt_X)[:, 1]
-    },
-    'Full Logistic Regression on UHBW': {
-        'model_name': 'Logistic Regression',
-        'model_data': 'Full',
-        'test_data': 'UHBW',
-        'y_true': base_full_uhbw_y,
-        'y_pred': base_eval_full.pipe.predict(base_full_uhbw_X),
-        'y_prob': base_eval_full.pipe.predict_proba(base_full_uhbw_X)[:, 1]
-    },
-    f'Full {best_eval_full.model_name} on NBT': {
-        'model_name': best_eval_full.model_name,
+    f'Full {full_eval_dict['Random Forest'].model_name} on NBT': {
+        'model_name': full_eval_dict['Random Forest'].model_name,
         'model_data': 'Full',
         'test_data': 'NBT',
         'y_true': best_full_nbt_y,
-        'y_pred': best_eval_full.pipe.predict(best_full_nbt_X),
-        'y_prob': best_eval_full.pipe.predict_proba(best_full_nbt_X)[:, 1]
+        'y_pred': full_eval_dict['Random Forest'].pipe.predict(best_full_nbt_X),
+        'y_prob': full_eval_dict['Random Forest'].pipe.predict_proba(best_full_nbt_X)[:, 1]
     },
-    f'Full {best_eval_full.model_name} on UHBW': {
-        'model_name': best_eval_full.model_name,
+    f'Full {full_eval_dict['Random Forest'].model_name} on UHBW': {
+        'model_name': full_eval_dict['Random Forest'].model_name,
         'model_data': 'Full',
         'test_data': 'UHBW',
         'y_true': best_full_uhbw_y,
-        'y_pred': best_eval_full.pipe.predict(best_full_uhbw_X),
-        'y_prob': best_eval_full.pipe.predict_proba(best_full_uhbw_X)[:, 1]
+        'y_pred': full_eval_dict['Random Forest'].pipe.predict(best_full_uhbw_X),
+        'y_prob': full_eval_dict['Random Forest'].pipe.predict_proba(best_full_uhbw_X)[:, 1]
     },
-    'NBT Logistic Regression on UHBW': {
-        'model_name': 'Logistic Regression',
+    f'NBT {nbt_eval_dict['LightGBM'].model_name}': {
+        'model_name': nbt_eval_dict['LightGBM'].model_name,
         'model_data': 'NBT',
-        'test_data': 'UHBW',
-        'y_true': base_eval_uhbw.y,
-        'y_pred': base_eval_nbt.pipe.predict(base_eval_uhbw.X),
-        'y_prob': base_eval_nbt.pipe.predict_proba(base_eval_uhbw.X)[:, 1]
-    },
-    f'NBT {best_eval_nbt.model_name} on UHBW': {
-        'model_name': best_eval_nbt.model_name,
-        'model_data': 'NBT',
-        'test_data': 'UHBW',
-        'y_true': best_eval_uhbw.y,
-        'y_pred': best_eval_nbt.pipe.predict(best_eval_uhbw.X),
-        'y_prob': best_eval_nbt.pipe.predict_proba(best_eval_uhbw.X)[:, 1]
-    },
-    'UHBW Logistic Regression on NBT': {
-        'model_name': 'Logistic Regression',
-        'model_data': 'UHBW',
         'test_data': 'NBT',
-        'y_true': base_eval_nbt.y,
-        'y_pred': base_eval_uhbw.pipe.predict(base_eval_nbt.X),
-        'y_prob': base_eval_uhbw.pipe.predict_proba(base_eval_nbt.X)[:, 1]
+        'y_true': nbt_eval_dict['LightGBM'].y,
+        'y_pred': nbt_eval_dict['LightGBM'].pipe.predict(nbt_eval_dict['LightGBM'].X),
+        'y_prob': nbt_eval_dict['LightGBM'].pipe.predict_proba(nbt_eval_dict['LightGBM'].X)[:, 1]
     },
-    f'UHBW {best_eval_uhbw.model_name} on NBT': {
-        'model_name': best_eval_uhbw.model_name,
+    f'UHBW {uhbw_eval_dict['LightGBM'].model_name}': {
+        'model_name': uhbw_eval_dict['LightGBM'].model_name,
         'model_data': 'UHBW',
-        'test_data': 'NBT',
-        'y_true': best_eval_nbt.y,
-        'y_pred': best_eval_uhbw.pipe.predict(best_eval_nbt.X),
-        'y_prob': best_eval_uhbw.pipe.predict_proba(best_eval_nbt.X)[:, 1]
+        'test_data': 'UHBW',
+        'y_true': uhbw_eval_dict['LightGBM'].y,
+        'y_pred': uhbw_eval_dict['LightGBM'].pipe.predict(uhbw_eval_dict['LightGBM'].X),
+        'y_prob': uhbw_eval_dict['LightGBM'].pipe.predict_proba(uhbw_eval_dict['LightGBM'].X)[:, 1]
     }
 }
 
