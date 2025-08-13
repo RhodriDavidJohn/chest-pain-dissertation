05c050ef39cf3a42489cb0e4d12998ddeb5fb36f
Rhodri Davies <rhodridavies99@yahoo.com>
2025-08-13 17:40:24 +0100

    Changed best NBT and UHBW models to be LightGBM according to results

--- code/model_evaluation.py
+++ code/model_evaluation.py
@@ -311,21 +311,21 @@ information_dict = {
         'y_pred': full_eval_dict['Random Forest'].pipe.predict(best_full_uhbw_X),
         'y_prob': full_eval_dict['Random Forest'].pipe.predict_proba(best_full_uhbw_X)[:, 1]
     },
-    f'NBT {nbt_eval_dict['Random Forest'].model_name}': {
-        'model_name': nbt_eval_dict['Random Forest'].model_name,
+    f'NBT {nbt_eval_dict['LightGBM'].model_name}': {
+        'model_name': nbt_eval_dict['LightGBM'].model_name,
         'model_data': 'NBT',
         'test_data': 'NBT',
-        'y_true': nbt_eval_dict['Random Forest'].y,
-        'y_pred': nbt_eval_dict['Random Forest'].pipe.predict(nbt_eval_dict['Random Forest'].X),
-        'y_prob': nbt_eval_dict['Random Forest'].pipe.predict_proba(nbt_eval_dict['Random Forest'].X)[:, 1]
+        'y_true': nbt_eval_dict['LightGBM'].y,
+        'y_pred': nbt_eval_dict['LightGBM'].pipe.predict(nbt_eval_dict['LightGBM'].X),
+        'y_prob': nbt_eval_dict['LightGBM'].pipe.predict_proba(nbt_eval_dict['LightGBM'].X)[:, 1]
     },
-    f'UHBW {uhbw_eval_dict['XGBoost'].model_name}': {
-        'model_name': uhbw_eval_dict['XGBoost'].model_name,
+    f'UHBW {uhbw_eval_dict['LightGBM'].model_name}': {
+        'model_name': uhbw_eval_dict['LightGBM'].model_name,
         'model_data': 'UHBW',
         'test_data': 'UHBW',
-        'y_true': uhbw_eval_dict['XGBoost'].y,
-        'y_pred': uhbw_eval_dict['XGBoost'].pipe.predict(uhbw_eval_dict['XGBoost'].X),
-        'y_prob': uhbw_eval_dict['XGBoost'].pipe.predict_proba(uhbw_eval_dict['XGBoost'].X)[:, 1]
+        'y_true': uhbw_eval_dict['LightGBM'].y,
+        'y_pred': uhbw_eval_dict['LightGBM'].pipe.predict(uhbw_eval_dict['LightGBM'].X),
+        'y_prob': uhbw_eval_dict['LightGBM'].pipe.predict_proba(uhbw_eval_dict['LightGBM'].X)[:, 1]
     }
 }
 
